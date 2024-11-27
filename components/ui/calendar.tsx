45875637f739be6d0fb462b4ac448ef483980aaa
Sateesh Varma <sateeshvarma73@gmail.com>
2024-11-27 16:27:18 +0530

    module fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,9 +54,29 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+        Nav: ({ onPreviousClick, onNextClick }) => (
+          <div className="flex items-center space-x-1">
+            <button
+              onClick={onPreviousClick}
+              className={cn(
+                buttonVariants({ variant: 'outline' }),
+                'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+              )}
+            >
+              <ChevronLeft className="h-4 w-4" />
+            </button>
+            <button
+              onClick={onNextClick}
+              className={cn(
+                buttonVariants({ variant: 'outline' }),
+                'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100'
+              )}
+            >
+              <ChevronRight className="h-4 w-4" />
+            </button>
+          </div>
+        ),
+      }}      
       {...props}
     />
   );
